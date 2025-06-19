7f6a9344a29de38f37ccd1fc2d0834e9eb5e9317
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-19 13:07:47 +0300

    emitting events with @Output

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, Input, input} from '@angular/core';
+import {Component, computed, EventEmitter, Input, input, Output} from '@angular/core';
 
 
 @Component({
@@ -9,17 +9,16 @@ import {Component, computed, Input, input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input<string>();
-  name = input.required<string>();
+  @Input({required: true}) id!:string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select= new EventEmitter();
 
-  imagePath = computed(() => 'assets/users/' + this.avatar());
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar();
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
+    this.select.emit(this.id)
   }
 }
