ee2527792472146bd9fbbeba47cbd02a73e43218
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-27 13:14:38 +0300

    completed switch to modules + standalone components

--- 03-essentials-modules/01-starting-project/src/app/app.component.ts
+++ 03-essentials-modules/01-starting-project/src/app/app.component.ts
@@ -1,16 +1,11 @@
-import { Component } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import {Component} from '@angular/core';
+import {DUMMY_USERS} from './dummy-users';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent]
+  selector: 'app-root',
+  standalone: false,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
