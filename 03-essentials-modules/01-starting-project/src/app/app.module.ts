a9dff7a04403b1c4cf7c18da29ee271bb236e7df
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-27 13:55:00 +0300

    created a reusable module

--- 03-essentials-modules/01-starting-project/src/app/app.module.ts
+++ 03-essentials-modules/01-starting-project/src/app/app.module.ts
@@ -8,18 +8,18 @@ import {CardComponent} from "./shared/card/card.component";
 import {NewTaskComponent} from "./tasks/new-task/new-task.component";
 import {FormsModule} from "@angular/forms";
 import {TaskComponent} from "./tasks/task/task.component";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule]
+  imports: [BrowserModule, FormsModule, SharedModule]
 })
 export class AppModule {
 
