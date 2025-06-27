48d2f6004a69234bd85c811bfb388bbf33553c83
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-27 13:37:41 +0300

    completed migration to modules

---
+++ 03-essentials-modules/01-starting-project/src/app/header/header.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from "@angular/core";
+import {HeaderComponent} from "./header.component";
+import {BrowserModule} from "@angular/platform-browser";
+
+@NgModule({
+  declarations: [HeaderComponent],
+  bootstrap: [HeaderComponent],
+})
+export class HeaderModule {
+}
--- 03-essentials-modules/01-starting-project/src/app/tasks/tasks.component.ts
+++ 03-essentials-modules/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,22 +1,19 @@
-import { Component, Input } from '@angular/core';
-
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { TasksService } from './tasks.service';
+import {Component, Input} from '@angular/core';
+import {TasksService} from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
+  standalone: false,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
-  @Input({ required: true }) userId!: string;
-  @Input({ required: true }) name!: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
   isAddingTask = false;
 
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {
+  }
 
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
