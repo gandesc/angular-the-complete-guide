5d5b317ce4d97c68664601fc4c29e2e33d4f80bd
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-30 12:22:09 +0300

    multiple modules

--- 03-essentials-modules/01-starting-project/src/app/app.module.ts
+++ 03-essentials-modules/01-starting-project/src/app/app.module.ts
@@ -1,25 +1,20 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {UserComponent} from "./user/user.component";
-import {TasksComponent} from "./tasks/tasks.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {HeaderComponent} from "./header/header.component";
-import {CardComponent} from "./shared/card/card.component";
-import {NewTaskComponent} from "./tasks/new-task/new-task.component";
 import {FormsModule} from "@angular/forms";
-import {TaskComponent} from "./tasks/task/task.component";
 import {SharedModule} from "./shared/shared.module";
+import {TasksModule} from "./tasks/tasks.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent],
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule]
+  imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
 
