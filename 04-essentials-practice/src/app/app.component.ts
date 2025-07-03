5e10414633f2d564e1996557282a0ded79ac2b85
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-07-03 12:25:37 +0300

    migrated to modules

--- 04-essentials-practice/src/app/app.component.ts
+++ 04-essentials-practice/src/app/app.component.ts
@@ -1,19 +1,11 @@
 import {Component, inject} from '@angular/core';
-import {HeaderComponent} from "./header/header.component";
-import {UserInputComponent} from "./user-input/user-input.component";
-import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
 import type {InvestmentInput} from "./user-input/investment-input.model";
 import {InvestmentService} from "./investment.service";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
-  imports: [
-    HeaderComponent,
-    UserInputComponent,
-    InvestmentResultsComponent
-  ]
 })
 export class AppComponent {
   private investmentService = inject(InvestmentService);
--- 04-essentials-practice/src/app/investment-results/investment-results.component.ts
+++ 04-essentials-practice/src/app/investment-results/investment-results.component.ts
@@ -4,10 +4,7 @@ import {InvestmentService} from "../investment.service";
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [
-    CurrencyPipe
-  ],
+  standalone: false,
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
