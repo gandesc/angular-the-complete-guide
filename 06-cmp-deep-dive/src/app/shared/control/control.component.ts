8b949f772cd7cabb5bb20f258bd086273956c27b
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-07-10 13:48:24 +0300

    host element injection example

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostBinding, HostListener, input, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, HostBinding, HostListener, inject, input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -19,8 +19,10 @@ export class ControlComponent {
   // }
 
   label = input.required<string>();
+  private el = inject(ElementRef);
 
   onClick() {
       console.log('clicked')
+      console.log(this.el)
     }
 }
